840fc2a7956db6ed23b635f41149560ff39114aa
joaogkvalho <joaogkvalho@gmail.com>
2024-06-19 09:29:04 -0300

    first screen of the app finished

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,26 @@
+import { TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+
+export type ButtonTypeStyleProps = 'PRIMARY' | 'SECONDARY'
+
+type Props = {
+    type: ButtonTypeStyleProps
+}
+
+export const Container = styled(TouchableOpacity)<Props>`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+
+    min-height: 56px;
+    max-height: 56px;
+
+    background-color: ${({ theme, type }) => type === 'PRIMARY' ? theme.COLORS.GREEN_700 : theme.COLORS.RED_DARK};
+    border-radius: 6px;
+`
+
+export const Title = styled.Text`
+    font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+    font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+    color: ${({ theme }) => theme.COLORS.WHITE};
+`
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,15 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { Button } from '../../components/Button';
 import { GroupCard } from '../../components/GroupCard';
 import { Header } from '../../components/Header';
 import { Highlight } from '../../components/Highlight';
+import { ListEmpty } from '../../components/ListEmpty';
 import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera do Ignite', 'Amigos'])
+
   return (
     <Container>
       <Header />
@@ -13,7 +19,25 @@ export function Groups() {
         subtitle="jogue com a sua turma"
       />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList 
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item} 
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message="Que tal cadastrar a primeira turma?" 
+          />
+        )}
+      />
+
+      <Button 
+        title="Criar nova turma"
+      />
     </Container>
   );
 }
